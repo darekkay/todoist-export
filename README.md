513600b7c9b93f277174be5daeacbe6c5cab6056
Darek Kay <darekkay@gmail.com>
2014-12-11 20:03:40 +0100

    Init project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Todoist Export
+
+A tool to backup Todoist data as JSON. Currently only active projects/tasks are supported.
+
+## License
+
+Copyright 2014 Darek Kay <darekkay@eclectide.com>  
+
+This project and its contents are open source under the [MIT license](LICENSE).
---
+++ app.js
@@ -0,0 +1,91 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var request = require('request');
+
+process.env['NODE_ENV'] = 'production';
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+    res.render('index', {});
+});
+
+function call(api, parameters, callback) {
+    request.post({
+        url: "https://api.todoist.com/" + api,
+        form: parameters,
+        json: true
+    }, callback)
+}
+
+function sendError(res, message) {
+    res.render("error", {
+        message: message
+    });
+}
+
+app.post("/todoist.json", function (req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
+    var exportAll = req.body.export === "all";
+
+    call("API/login", {email: email, password: password}, function (err, httpResponse, body) {
+
+        throw new Error("muh");
+
+        if (body === "LOGIN_ERROR") {
+            sendError(res, "Incorrect email or password");
+            return;
+        }
+
+        var token = body["api_token"];
+        if (token === undefined) {
+            sendError(res, "Couldn't get API token");
+            return;
+        }
+
+        call("TodoistSync/v5.3/get", {api_token: token, seq_no: 0}, function (err, httpResponse, body) {
+            if (exportAll) {
+                res.json(body);
+            }
+            else {
+                res.json(body.Items)
+            }
+        });
+    });
+});
+
+app.get('*', function (req, res) {
+    res.redirect("/");
+});
+
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message
+    });
+});
+
+module.exports = app;
