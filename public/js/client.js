97298396b260288468d97a22a7417d413ed14728
Darek Kay <darekkay@gmail.com>
2019-04-26 23:31:45 +0200

    Remove external scripts and stylesheets

--- public/js/client.js
+++ public/js/client.js
@@ -1,16 +1,18 @@
-$(document).ready(function () {
+window.onload = function() {
+  var params = {};
+  location.search.substr(1).split("&").forEach(function(item) {
+    var pair = item.split("="), key = pair[0],
+      value = pair[1] && decodeURIComponent(pair[1]);
+    (params[key] = params[key] || []).push(value);
+  });
 
-    $("input[name=export]:radio").change(function () {
-
-        var showFormat = $("input[name=export]:checked").val() === "tasks";
-
-        if (showFormat) {
-            $("#format").slideDown();
-        }
-        else {
-            $("#format").slideUp();
-        }
-
-    });
-
-});
+  if (params.code) {
+    window.location.replace("/todoist-export/export?code=" + params.code + "&format=" + params.state);
+  } else if (params.token) {
+    document.querySelector("#persistentBackup").style.display = "block";
+    var persistentBackupUrl = window.location.href.split("?")[0] + "download?token=" + params.token + "&format=" + params.format;
+    document.querySelector("#persistentBackupUrl").href = persistentBackupUrl;
+    document.querySelector("#persistentBackupUrl").innerText = persistentBackupUrl;
+    window.location.replace(persistentBackupUrl);
+  }
+};
